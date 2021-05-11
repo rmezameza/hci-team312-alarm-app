8e5e96e148ca7a83e7e5319ea1011cd570685b2e
Roberto Alfredo Meza Meza <61918176+rmezameza@users.noreply.github.com>
2021-05-11 22:46:39 +0200

    Initialize project

---
+++ app/src/main/java/at/ac/univie/hci/u_alarm/MainActivity.java
@@ -0,0 +1,14 @@
+package at.ac.univie.hci.u_alarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
