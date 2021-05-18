37a998729b67d8258b0447f23b5de240db61781a
Roberto Alfredo Meza Meza <mezamezar87@univie.ac.at>
2021-05-18 12:24:59 +0200

    Added PDFViewer and pdf for building map.

--- app/src/main/java/at/ac/univie/hci/u_alarm/ui/map/BuildingMapFragment.java
+++ app/src/main/java/at/ac/univie/hci/u_alarm/ui/map/BuildingMapFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
 
 import androidx.fragment.app.Fragment;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 import at.ac.univie.hci.u_alarm.R;
+import at.ac.univie.hci.u_alarm.databinding.FragmentBuildingMapBinding;
 
 
 public class BuildingMapFragment extends Fragment {
@@ -17,22 +19,21 @@ public class BuildingMapFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ID = "2";
 
-
-    public BuildingMapFragment() {
-
-    }
+    private FragmentBuildingMapBinding binding;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        this.binding = FragmentBuildingMapBinding.inflate(inflater, container, false);
+        View rootBuldingMap = binding.getRoot();
+
         // Inflate the layout for this fragment
-
-
-        WebView pdfView = (WebView)getView().findViewById(R.id.childFragment_building_map);
-        pdfView.loadUrl("https://www.univie.ac.at/fileadmin/user_upload/startseite/Fotos/Plaene/plan-hauptgebaeude-universitaet-wien-2020-v1.pdf");
-
-        return inflater.inflate(R.layout.fragment_building_map, container, false);
+        PDFView pdfView = (PDFView)rootBuldingMap.findViewById(R.id.childFragment_building_map);
+        //Uri buildingMap = Uri.parse("https://www.univie.ac.at/fileadmin/user_upload/startseite/Fotos/Plaene/plan-hauptgebaeude-universitaet-wien-2020-v1.pdf");
+        pdfView.fromAsset("plan-hauptgebaeude-universitaet-wien-2020-v1.pdf").load();
+        return rootBuldingMap;
     }
 
     /*
