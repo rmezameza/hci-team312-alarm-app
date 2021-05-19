f59e95603923b336b8d1889715ca8d34261b4656
Roberto Alfredo Meza Meza <mezamezar87@univie.ac.at>
2021-05-20 01:58:04 +0200

    Change Design v2 and added content for home fragment
    Changed app icon. Added content to home fragment and also a background image / header image. Changed primary color in night theme. I changed in fragment_configuration.xml the text views' width to match_parent. It should be more responsive now. Also added picasso library for image.

--- app/src/main/java/at/ac/univie/hci/u_alarm/ui/home/HomeFragment.java
+++ app/src/main/java/at/ac/univie/hci/u_alarm/ui/home/HomeFragment.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.squareup.picasso.Picasso;
 
 import at.ac.univie.hci.u_alarm.R;
 import at.ac.univie.hci.u_alarm.databinding.FragmentHomeBinding;
@@ -25,16 +26,47 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
+        // Inflate the layout of this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        // Background image
+        final ImageView backgroundImage = binding.backgroundImage;
+        Picasso.get().load(R.drawable.u_alarm_logo).into(backgroundImage);
+
+
+        // Text for the home fragment
+        // Content of TextViews are taken from the HomeViewModel
+        final TextView tvActualPlaceTitle = binding.textActualPlaceTitle;
+        homeViewModel.getActualPlaceTitle()
+                .observe(getViewLifecycleOwner(), tvActualPlaceTitle::setText);
+
+
+        final TextView tvActualPlaceContent = binding.textActualPlaceContent;
+        homeViewModel.getActualPlaceContent()
+                .observe(getViewLifecycleOwner(), tvActualPlaceContent::setText);
+
+
+        final TextView tvTelephoneTitle = binding.textTelephoneTitle;
+        homeViewModel.getTelephoneTitle()
+                .observe(getViewLifecycleOwner(), tvTelephoneTitle::setText);
+
+
+        final TextView tvTelephoneContent = binding.textTelephoneContent;
+        homeViewModel.getTelephoneContent()
+                .observe(getViewLifecycleOwner(), tvTelephoneContent::setText);
+
+
+        final TextView tvEmailTitle = binding.textEmailTitle;
+        homeViewModel.getEmailTitle()
+                .observe(getViewLifecycleOwner(), tvEmailTitle::setText);
+
+
+        final TextView tvEmailContent = binding.textEmailContent;
+        homeViewModel.getEmailContent()
+                .observe(getViewLifecycleOwner(), tvEmailContent::setText);
+
+
         return root;
     }
 
--- app/src/main/java/at/ac/univie/hci/u_alarm/ui/home/HomeViewModel.java
+++ app/src/main/java/at/ac/univie/hci/u_alarm/ui/home/HomeViewModel.java
@@ -6,14 +6,39 @@ import androidx.lifecycle.ViewModel;
 
 public class HomeViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private final MutableLiveData<String> actualPlaceTitle;
+    private final MutableLiveData<String> actualPlaceContent;
+    private final MutableLiveData<String> telephoneTitle;
+    private final MutableLiveData<String> telephoneContent;
+    private final MutableLiveData<String> emailTitle;
+    private final MutableLiveData<String> emailContent;
 
     public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        this.actualPlaceTitle = new MutableLiveData<>();
+        this.actualPlaceContent = new MutableLiveData<>();
+        this.telephoneTitle = new MutableLiveData<>();
+        this.telephoneContent = new MutableLiveData<>();
+        this.emailTitle = new MutableLiveData<>();
+        this.emailContent = new MutableLiveData<>();
+
+        this.actualPlaceTitle.setValue("Aktueller Ort");
+        this.actualPlaceContent.setValue("Hauptgebäude der Universtität Wien");
+
+        this.telephoneTitle.setValue("Telefon");
+        this.telephoneContent.setValue("+43-1-4277-128 81\n" +
+                "+43-1-4277-128 82");
+
+        this.emailTitle.setValue("E-Mail");
+        this.emailContent.setValue("siwachthg1(at)univie.ac.at\n" +
+                "siwachthg2(at)univie.ac.at");
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<String> getActualPlaceTitle() {
+        return this.actualPlaceTitle;
     }
+    public LiveData<String> getActualPlaceContent() { return this.actualPlaceContent; }
+    public LiveData<String> getTelephoneTitle() { return this.telephoneTitle; }
+    public LiveData<String> getTelephoneContent() { return this.telephoneContent; }
+    public LiveData<String> getEmailTitle() { return this.emailTitle; }
+    public LiveData<String> getEmailContent() { return this.emailContent; }
 }
