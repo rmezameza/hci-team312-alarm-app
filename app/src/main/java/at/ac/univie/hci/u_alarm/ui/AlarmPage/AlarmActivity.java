455bff30abe276e8cbd9fe6a06626c9d6e4e2ff6
Roberto Alfredo Meza Meza <mezamezar87@univie.ac.at>
2021-06-06 23:39:34 +0200

    Update AlarmActivity.java

--- app/src/main/java/at/ac/univie/hci/u_alarm/ui/AlarmPage/AlarmActivity.java
+++ app/src/main/java/at/ac/univie/hci/u_alarm/ui/AlarmPage/AlarmActivity.java
@@ -1,83 +1,92 @@
-package at.ac.univie.hci.u_alarm.ui.AlarmPage;
-
-import androidx.appcompat.app.AppCompatActivity;
+package at.ac.univie.hci.u_alarm.ui.alarmpage;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
+
 
 import at.ac.univie.hci.u_alarm.MainActivity;
 import at.ac.univie.hci.u_alarm.R;
-import at.ac.univie.hci.u_alarm.ui.alarmlist.Alarm;
 
 public class AlarmActivity extends AppCompatActivity {
 
-    public static  ArrayList<String> alarmtypeGlobal =new ArrayList<>();
-    public static    ArrayList<String>  alarmortGlobal =new ArrayList<>();
-    public static  ArrayList alarmdateGlobal = new ArrayList<String>();
 
-    Button stop_button;
-    TextView alarmname ;
-    TextView ort ;
+
+    private static final String ALARM_NAME = "Feueralarm";
+    private static final String ALARM_PLACE = "Erdgeschoss";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
 
-        stop_button = (Button) findViewById(R.id.stop_button);
-        alarmname = findViewById(R.id.alarm_name);
-        ort = findViewById(R.id.alarm_ort_in_alarm_page);
+        Button stopButton;
+        TextView tvAlarmName;
+        TextView tvAlarmPlace;
+        ImageButton mapButton;
 
-        stop_button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AlarmActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
+        stopButton = findViewById(R.id.stop_button);
+        mapButton = findViewById(R.id.map_button);
+        tvAlarmName = findViewById(R.id.alarm_name);
+        tvAlarmPlace = findViewById(R.id.alarm_place_name);
+
+
+        AlarmSignaler alarmTester = new AlarmSignaler(AlarmActivity.this.getApplicationContext(),
+                500,
+                255,
+                10,
+                500,
+                10);
+
+        Thread thread = new Thread(alarmTester::startAlarm);
+        thread.start();
+
+
+        stopButton.setOnClickListener(v -> {
+            alarmTester.stopAlarm();
+            Intent intent = new Intent(AlarmActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        mapButton.setOnClickListener(v -> {
+            alarmTester.stopAlarm();
+
+            Intent intent = new Intent(AlarmActivity.this, MainActivity.class);
+            String goToMap = "gotomap";
+            intent.putExtra("GO_TO_MAP", goToMap);
+            startActivity(intent);
         });
 
 
-        Random random = new Random();
 
-        ArrayList<String>  alarmtype =new ArrayList<>(Arrays.asList(
-                "Feuer Alarm","Probe Alarm",
-                "Alarm1","Alarm2"
-        ));
-        ArrayList<String>  alarmort =new ArrayList<>(Arrays.asList(
-                "Erdgeschoss","zweite Stock ",
-                "erste Stock","Garten"
-        ));
+        tvAlarmName.setText(ALARM_NAME);
+        tvAlarmPlace.setText(ALARM_PLACE);
 
-        //Alarmtype und Ort wird gewählt
-        int random_Number_Alarmtype= random.nextInt(alarmtype.size());
-        int random_Number_Alarmort= random.nextInt(alarmort.size());
+        // Save the Details of the alarm in arrays in MainActivity
+        MainActivity.alarmTypes.add(ALARM_NAME);
+        MainActivity.alarmPlaces.add(ALARM_PLACE);
+        MainActivity.alarmDates.add(calculateDate());
 
-        alarmname.setText(alarmtype.get(random_Number_Alarmtype));
-        ort.setText(alarmort.get(random_Number_Alarmort));
 
-        //Datum wird geholt und in einem List einfügen
-        DateTimeFormatter jahr_monat_tag = DateTimeFormatter.ofPattern("uuuu/MM/dd");
+    }
+
+    // Set date (actual date and time) for alarm
+    private static String calculateDate() {
+
+        // Get date and time. Convert them to strings and assign it to alarmTime.
+        DateTimeFormatter date = DateTimeFormatter.ofPattern("uuuu/MM/dd");
         LocalDate localDate = LocalDate.now();
-        DateTimeFormatter zeit = DateTimeFormatter.ofPattern("HH:mm:ss");
+        DateTimeFormatter time = DateTimeFormatter.ofPattern("HH:mm:ss");
         LocalTime localTime = LocalTime.now();
-        String date = jahr_monat_tag.format(localDate) +" " + zeit.format(localTime) ;
-
-        Alarm list = new Alarm(alarmname.getText().toString(),ort.getText().toString(), date);
-
-        alarmtypeGlobal.add(alarmname.getText().toString());
-        alarmortGlobal.add(ort.getText().toString());
-        alarmdateGlobal.add(date);
-
-
+        return date.format(localDate) + " | " + time.format(localTime);
     }
 }
